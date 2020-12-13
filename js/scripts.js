43ad8efde75d5a82971dbc035b5d3595b3af942b
tomivm <tomivm98@gmail.com>
2020-12-13 13:16:17 -0300

    Areas finalizadas con modal, actualizar descripciones

--- js/scripts.js
+++ js/scripts.js
@@ -217,7 +217,7 @@ $(window).on("load",function (){
                 data: $(this).serialize(),
                 success: function (data)
                 {
-                    var messageAlert = 'alert-' + data.type;
+                    var messageAlert = 'alert-'+ data.type;
                     var messageText = data.message;
 
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
